d7375c7c6c01610509ec58c74d871369381d5d7d
justyn1303 <justyn1303@gmail.com>
2022-12-12 15:09:09 +0100

    12,12 v2

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,24 @@ app.listen(port, () => {
   console.log(`serve at http://localhost:${port}`);
 });
 
+app.get("/api/products/slug/:slug", (req, res) => {
+  const product = data.products.find((x) => x.slug === req.params.slug);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product Not Found" });
+  }
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product Not Found" });
+  }
+});
+
 // dotenv.config();
 
 // mongoose
