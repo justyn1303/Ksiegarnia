106756e0a8bf1eeb802cc54acec9ee7e89d75874
snix422 <102601543+snix422@users.noreply.github.com>
2022-12-28 17:07:52 +0100

    28.12.2022r

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -10,10 +10,8 @@ const productSchema = new mongoose.Schema(
     price: { type: Number, required: true },
     category: { type: String, required: true },
     description: { type: String, required: true },
-    rating: { type: Number, required: true },
     yearOfPublication: { type: Number, required: true },
     countInStock: { type: Number, required: true },
-    numReviews: { type: Number, required: true },
     brand: { type: String, required: true },
   },
   {
